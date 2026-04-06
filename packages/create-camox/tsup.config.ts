b85b85c28a4ab4ea4cd908e8cf1ef3a7fd1f7e70
Rémi de Juvigny <8087692+remidej@users.noreply.github.com>
2026-04-06 23:35:46 +0200

    chore: delete unused create-camox package

--- packages/create-camox/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  outDir: "dist",
-  clean: true,
-  banner: { js: "#!/usr/bin/env node" },
-});
