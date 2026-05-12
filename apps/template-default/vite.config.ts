48bc894938f78bbc3a8f16e9c1f787d0ea26381e
Rémi de Juvigny <8087692+remidej@users.noreply.github.com>
2026-05-12 10:39:04 +0200

    chore: restore nitro plugin

--- apps/template-default/vite.config.ts
+++ apps/template-default/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { camox } from "camox/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig, loadEnv } from "vite-plus";
 
 const env = loadEnv(process.env.NODE_ENV!, process.cwd(), "CAMOX_");
@@ -11,6 +12,7 @@ export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     tailwindcss(),
+    nitro(),
     camox({
       projectSlug: "camox-template-default-01", // camox-cli:replace-slug
       syncSecret: env.CAMOX_SYNC_SECRET,
